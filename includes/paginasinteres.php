e4732feb81c77df9f73d558dd5c15c6a92e54ae0
stratooz <stratooz@hotmail.com>
2014-11-14 23:19:58 -0300

    correccion de link
    se corrigen los links

--- includes/paginasinteres.php
+++ includes/paginasinteres.php
@@ -1,7 +1,13 @@
-<div class="section group example">
-					<a href="#"> Punto Motel</a><br /><br />
-                    <a href="#"> Busca tu Celu</a><br /><br />
-                    <a href="#"> TurisChile</a><br /><br />
-                    <a href="#"> </a>
-					<div class="clear"></div>
+<div class="section group example">
+
+					<a href="http://puntomhotel.esy.es"> Place Love</a><br /><br />
+
+                    <a href="www.buscatucelu.yzi.me"> Busca tu Celu</a><br /><br />
+
+                    <a href="www.turischile.uni.me"> TurisChile</a><br /><br />
+
+                    <a href="#"> </a>
+
+					<div class="clear"></div>
+
       </div>
