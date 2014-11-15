e43f311bb9a4e2ec823565e47410d9f27fa511ba
stratooz <stratooz@hotmail.com>
2014-11-15 00:44:31 -0300

    cambio de datos de conexion
    se realizan los cambios de datos para poder probar la conexion a nivel
    local

--- codigosPHP/conexion.php
+++ codigosPHP/conexion.php
@@ -1,6 +1,6 @@
 <? 
 
-$conexion=mysql_connect("mysql.webcindario.com","infocar3","homero01") or die ("no hay conexion");
+$conexion=mysql_connect("localhost","root","") or die ("no hay conexion");
 
 $conectBD=mysql_select_db("infocar3",$conexion) or die ("no existe la BD");
 
