04c9d8a39d910f6fc3a28bc1b168e2a3dc78a050
stratooz <stratooz@hotmail.com>
2014-11-18 04:14:30 -0300

    se corrigen los links

--- includes/paginasinteres.php
+++ includes/paginasinteres.php
@@ -2,9 +2,9 @@
 
 					<a href="http://puntomhotel.esy.es"> Place Love</a><br /><br />
 
-                    <a href="www.buscatucelu.yzi.me"> Busca tu Celu</a><br /><br />
+                    <a href="http://www.buscatucelu.yzi.me"> Busca tu Celu</a><br /><br />
 
-                    <a href="www.turischile.uni.me"> TurisChile</a><br /><br />
+                    <a href="http://www.turischile.uni.me"> TurisChile</a><br /><br />
 
                     <a href="#"> </a>
 
