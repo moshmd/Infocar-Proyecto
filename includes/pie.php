538c7ea4f60f6648ba7c1d837a8eb032038de349
stratooz <stratooz@hotmail.com>
2014-11-18 04:44:43 -0300

    se agrega al nico como programador

--- includes/pie.php
+++ includes/pie.php
@@ -1,5 +1,9 @@
-<div class="footer-bottom" align="center">
-			<div class="copy">
-				<p align="center"><destacada>PROGRAMACION:</destacada>LEONARDO BASTIAS |<destacada> DISEÑO: </destacada>HECTOR CORTES</p>
-			</div>
+<div class="footer-bottom" align="center">
+
+			<div class="copy">
+
+				<p align="center"><destacada>PROGRAMACION:</destacada>LEONARDO BASTIAS - NICOLAS MONTERO |<destacada> DISEÑO: </destacada>HECTOR CORTES</p>
+
+			</div>
+
 		</div>
